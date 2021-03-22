b72f5f690b31b77548c0fd13e92f02763cd9338b
okjanu31 <valikasim14@gmail.com>
2021-03-22 06:16:32 +0000

    formatting all .p files tab on the EOD checking travis ci

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,2 +1,3 @@
 def add(x, y):
+    """Method syntax"""
     return x + y
--- app/app/tests.py
+++ app/app/tests.py
@@ -2,7 +2,9 @@ from django.test import TestCase
 
 from app.calc import add
 
+
 class CalcTests(TestCase):
 
-    def test_add_numbers(self):
-        self.assertEqual(add(3,7),10)
+        def test_add_numbers(self):
+            """sdfxdfgd"""
+            self.assertEqual(add(3, 7), 10)
